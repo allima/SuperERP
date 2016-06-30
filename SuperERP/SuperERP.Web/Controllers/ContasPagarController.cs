d43f8d411f54ecd73841148764322ce029074603
babuhzera <victorcabelo@gmail.com>
2016-06-29 21:13:05 -0300

    update controller layout

---
+++ SuperERP/SuperERP.Web/Controllers/ContasPagarController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SuperERP.Web.Controllers
+{
+    public class ContasPagarController : Controller
+    {
+        //
+        // GET: /ContasPagar/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
