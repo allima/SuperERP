00b9fe6a1190cc3f4d04143e6f48357697325e28
Alexandre de Lima <allima1991@gmail.com>
2016-06-30 18:18:16 -0300

    login tentativa

---
+++ SuperERP/SuperERP.DAL/Repositories/RepositorioCookies.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace SuperERP.DAL.Repositories
+{
+   public class RepositorioCookies
+    {
+        public static void RegitraCookie(long IDUsuario)
+        {
+            HttpCookie UserCookie = new HttpCookie("CookieUsuario");
+            UserCookie.Values["IDUsuario"] = IDUsuario.ToString();
+            UserCookie.Expires = DateTime.Now.AddDays(1);
+            HttpContext.Current.Response.Cookies.Add(UserCookie);
+        }
+    }
+}
---
+++ SuperERP/SuperERP.Web/Filtro/Filtros.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SuperERP.Web.Filtro
+{
+    [HandleError]
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = true, AllowMultiple = true)]
+    public class FiltroAcesso : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext FiltroDeContexto)
+        {
+            var Controller = FiltroDeContexto.ActionDescriptor.ControllerDescriptor.ControllerName;
+            var Action = FiltroDeContexto.ActionDescriptor.ActionName;
+
+            if (Controller != "Home" || Action != "Login")
+            {
+                if (RepositorioUsuarios.VerificaStatusUsuario() == null)
+                {
+                    FiltroDeContexto.RequestContext.HttpContext.Response.Redirect(
+                        "Home/Login?Url=" + FiltroDeContexto.HttpContext.Request.Url.LocalPath);
+                }
+            }
+        }
+    }
+}
